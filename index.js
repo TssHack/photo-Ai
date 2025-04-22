ea7677c555ca2aadc68597e85eff2b080a924ece
Ehsan <144959137+TssHack@users.noreply.github.com>
2025-04-23 00:38:02 +0330

    Update index.js

--- index.js
+++ index.js
@@ -4,27 +4,28 @@ const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', async (req, res) => {
-  const { prompt, seed = 40, model = 'unity', width = 1024, height = 1024, safe = true, enhance = true } = req.query;
+app.use(express.json());
+
+app.all('/', async (req, res) => {
+  const method = req.method;
+  const params = method === 'GET' ? req.query : req.body;
+
+  const { prompt, seed = 40, model = 'unity', width = 1024, height = 1024, safe = true, enhance = true } = params;
 
   if (!prompt) {
-    return res.status(400).json({
-      code: 400,
-      owner: 't.me/abj0o',
-      message: 'Prompt is required.'
-    });
+    return res.status(400).json({ code: 400, owner: 't.me/haji_nuII', message: 'Prompt is required.' });
   }
 
   try {
     const translate = async (text, from = 'fa', to = 'en') => {
-      const params = new URLSearchParams({ sl: from, tl: to, q: text });
+      const data = new URLSearchParams({ sl: from, tl: to, q: text });
       const headers = {
         'User-Agent': 'AndroidTranslate/5.3.0.RC02.130475354-53000263 5.1 phone TRANSLATE_OPM5_TEST_1',
         'Content-Type': 'application/x-www-form-urlencoded',
       };
       const response = await axios.post(
         'https://translate.google.com/translate_a/single?client=at&dt=t&dt=ld&dt=qca&dt=rm&dt=bd&dj=1&hl=es-ES&ie=UTF-8&oe=UTF-8&inputm=2&otf=2&iid=1dd3b944-fa62-4b55-b330-74909a99969e',
-        params.toString(),
+        data.toString(),
         { headers }
       );
       return response.data.sentences[0].trans;
@@ -38,13 +39,7 @@ app.get('/', async (req, res) => {
     const buffer = Buffer.from(response.data);
 
     if (buffer.length < 55) {
-      return res.status(504).json({
-        code: 504,
-        owner: 't.me/abj0o',
-        prompt: translatedPrompt,
-        settings: req.query,
-        image: 'Error: No valid image received.'
-      });
+      return res.status(504).json({ code: 504, owner: 't.me/haji_nuII', prompt: translatedPrompt, settings: params, image: 'Error: No valid image received.' });
     }
 
     res.set('Content-Type', 'image/jpeg');
